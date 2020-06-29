9c43670e420a2dae5eea2218ec842e8fef88d9c5
Edin Jašarević <edinjasar14@gmail.com>
2020-06-29 14:56:48 +0200

    adding a site header

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,15 +6,9 @@ import { Component } from '@angular/core';
     <div>
       <h1>Upcoming Angular Events</h1>
       <hr/>
-      <div class="well">
-        <div>Hello World</div>
-      </div>
       <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
     </div>
-  `,
-  styles: [`
-    .well div { color: red; }
-  `]
+  `
 })
 export class EventsListComponent {
   event1 = {
