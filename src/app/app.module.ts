321906ccfca3ac7eb6997c3e5cc91dbf1c1afe40
Edin Jašarević <edinjasar14@gmail.com>
2020-06-30 14:07:02 +0200

    organizing your exports with barrets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index';
+
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
-import { CreateEventComponent } from './events/create-event.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { Error404Component } from './errors/404.component';
-import { EventListResolver } from './events/event-list-resolver.service';
+import { EventsAppComponent } from './events-app.component';
 
 @NgModule({
   imports: [
---
+++ src/app/events/event-details/index.ts
@@ -0,0 +1,4 @@
+import { from } from 'rxjs';
+
+export * from './event-details.component';
+export * from './event-route-activator.service';
