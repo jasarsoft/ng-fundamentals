78765b2821533792dd0391d0b08f5a300a5e33e2
Edin Jašarević <edinjasar14@gmail.com>
2020-06-30 13:24:53 +0200

    pre-loading data for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { appRoutes } from './routes';
 import { CreateEventComponent } from './events/create-event.component';
 import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { Error404Component } from './errors/404.component';
+import { EventListResolver } from './events/event-list-resolver.service';
 
 @NgModule({
   imports: [
@@ -31,6 +32,7 @@ import { Error404Component } from './errors/404.component';
   providers: [
     EventService,
     ToastrService,
+    EventListResolver,
     EventRouteActivator,
     {
       provide: 'canDeactivateCreateEvent',
