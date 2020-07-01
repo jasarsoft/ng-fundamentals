561990cc11e2c39aed83b7ab57aad908527998d8
Edin Jašarević <edinjasar14@gmail.com>
2020-07-01 10:03:00 +0200

    diving deeper into template-based form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,10 +18,13 @@ import { appRoutes } from './routes';
 import { Error404Component } from './errors/404.component';
 import { EventsAppComponent } from './events-app.component';
 import { AuthService } from './user/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   declarations: [
--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -1,23 +1,24 @@
 import { Component } from "@angular/core";
 import { Router } from '@angular/router';
+import { EventService } from './shared';
 
 @Component({
-  template: `
-    <h1>New Event</h1>
-    <hr>
-    <div class="col-md-6">
-      <h3>[Create Event Form will go here]</h3>
-      <br/>
-      <br/>
-      <button type="submit" class="btn btn-primary">Save</button>
-      <button type="button" class="btn btn-default" (click)="cancel()">Cancel</button>
-    </div>
-  `
+  templateUrl: './create-event.component.html',
+  styles: [`
+    em { float: right; color: #E05C67; padding-left: 10px; }
+    .error input { background-color: #E3C3C5; }
+  `]
 })
 export class CreateEventComponent {
+  newEvent;
   isDirty: boolean = true;
-  constructor(private router: Router) {
+  constructor(private router: Router, private eventService: EventService) {
 
+  }
+
+  saveEvent(formValues) {
+    this.eventService.saveEvent(formValues);
+    this.router.navigate(['/events'])
   }
 
   cancel() {
