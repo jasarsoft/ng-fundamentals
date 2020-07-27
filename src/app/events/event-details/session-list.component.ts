358e125f4fb683f1ea7b4d6cc1722654bdb0f811
Edin Jašarević <edinjasar14@gmail.com>
2020-07-27 20:46:47 +0200

    sorting data

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -9,6 +9,7 @@ import { ISession } from '../shared';
 export class SessionListComponent implements OnInit, OnChanges {
   @Input() sessions: ISession[];
   @Input() filterBy: string;
+  @Input() sortBy: string;
   visibleSessions: ISession[] = [];
 
   constructor() { }
@@ -18,6 +19,9 @@ export class SessionListComponent implements OnInit, OnChanges {
   ngOnChanges() {
     if (this.sessions) {
       this.filterSessions(this.filterBy);
+      this.sortBy === 'name'
+        ? this.visibleSessions.sort(sortByNameAsc)
+        : this.visibleSessions.sort(sortByVotesDesc);
     }
   }
 
@@ -30,4 +34,14 @@ export class SessionListComponent implements OnInit, OnChanges {
       });
     }
   }
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession) {
+  if(s1.name > s2.name) return 1
+  else if (s1.name == s2.name) return 0
+  else return -1;
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+  return s2.voters.length - s1.voters.length;
 }
