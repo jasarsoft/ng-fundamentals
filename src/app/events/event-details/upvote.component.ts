4f0fc46253f181c37d72a956337c1d3ccbaf76db
Edin Jašarević <edinjasar14@gmail.com>
2020-08-22 22:33:34 +0200

    using input setters

--- src/app/events/event-details/upvote.component.ts
+++ src/app/events/event-details/upvote.component.ts
@@ -4,24 +4,27 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   selector: 'upvote',
   styleUrls: ['./upvote.component.css'],
   template: `
-    <div class="votingWidgetContainer pointable" (click)="onClick()">
-      <div class="well votingWidget">
-        <div class="votingButton">
-          <i *ngIf="voted" class="glyphicon glyphicon-heart"></i>
-          <i *ngIf="!voted" class="glyphicon glyphicon-heart-empty"></i>
-        </div>
-        <div class="badge badge-inverse votingCount">
-          <div>{{count}}</div>
-        </div>
+  <div class="votingWidgetContainer pointable" (click)="onClick()">
+    <div class="well votingWidget">
+      <div class="votingButton">
+        <i class="glyphicon glyphicon-heart" [style.color]="iconColor"></i>
+      </div>
+      <div class="badge badge-inverse votingCount">
+        <div>{{count}}</div>
       </div>
     </div>
+  </div>
   `
 })
 
 export class UpvoteComponent implements OnInit {
   @Input() count: number;
-  @Input() voted: boolean;
+  @Input() set voted(val) {
+    console.log(val);
+    this.iconColor = val ? 'red' : 'white';
+  }
   @Output() vote = new EventEmitter();
+  iconColor: string;
 
   constructor() { }
 
