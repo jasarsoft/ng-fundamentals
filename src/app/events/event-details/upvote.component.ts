4a2ed3b57203231333a8c67adcc298d56147f5e1
Edin Jašarević <edinjasar14@gmail.com>
2020-11-05 22:13:52 +0100

    using delete

--- src/app/events/event-details/upvote.component.ts
+++ src/app/events/event-details/upvote.component.ts
@@ -1,33 +1,28 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Input, Output, Component, EventEmitter } from "@angular/core";
 
 @Component({
   selector: 'upvote',
   styleUrls: ['./upvote.component.css'],
-  template: `
-  <div class="votingWidgetContainer pointable" (click)="onClick()">
-    <div class="well votingWidget">
-      <div class="votingButton">
-        <i class="glyphicon glyphicon-heart" [style.color]="iconColor"></i>
-      </div>
-      <div class="badge badge-inverse votingCount">
-        <div>{{count}}</div>
+  template:`
+    <div class="votingWidgetContainer pointable" (click)="onClick()">
+      <div class="well votingWidget">
+        <div class="votingButton">
+          <i class="glyphicon glyphicon-heart" [style.color]="iconColor"></i>
+        </div>
+        <div class="badge badge-inverse votingCount">
+          <div>{{count}}</div>
+        </div>
       </div>
     </div>
-  </div>
   `
 })
-
-export class UpvoteComponent implements OnInit {
+export class UpvoteComponent {
   @Input() count: number;
   @Input() set voted(val) {
     this.iconColor = val ? 'red' : 'white';
   }
   @Output() vote = new EventEmitter();
   iconColor: string;
-
-  constructor() { }
-
-  ngOnInit() { }
 
   onClick() {
     this.vote.emit({});
