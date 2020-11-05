a790f7c34abb58c019b4663eb618cfb1fb665581
Edin Jašarević <edinjasar14@gmail.com>
2020-11-05 21:53:01 +0100

    using querystring parameters

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -31,27 +31,9 @@ export class EventService {
     EVENTS[index] = event;
   }
 
-  searchSessions(searchTerm: string) {
-    let term = searchTerm.toLocaleLowerCase();
-    let results: ISession[] = [];
-
-    EVENTS.forEach(event => {
-      let matchingSesssions = event.sessions.filter(session =>
-        session.name.toLocaleLowerCase().indexOf(term) > -1);
-
-      matchingSesssions = matchingSesssions.map((session: any) => {
-          session.eventId = event.id;
-          return session;
-        });
-      results = results.concat(matchingSesssions);
-    });
-
-    const emitter = new EventEmitter(true);
-    setTimeout(() => {
-      emitter.emit(results);
-    }, 100);
-
-    return emitter;
+  searchSessions(searchTerm: string): Observable<ISession[]> {
+    return this.http.get<ISession[]>('/api/sessions/search?search=' + searchTerm)
+      .pipe(catchError(this.handleError<ISession[]>('searchSessions')));
   }
 
   private handleError<T> (opertaion = 'operation', result?: T) {
